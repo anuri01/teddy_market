3ed44fc83ebfabe36b83e606dd0410fb7954c6aa
anuri01 <yang1234@gmail.com>
2025-08-18 15:53:44 +0900

    메시지 테스트

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function LoginPage() {
     return (
         <>
-        <h2>회원가입페이지</h2>
-        <p>회원가입 페이지 내용이 여기에 표시될 예정입니다.</p>
+        <h2>로그인 페이지</h2>
+        <p>로그인 페이지가 여기에 표시됩니다.</p>
         </>
     );
 }
