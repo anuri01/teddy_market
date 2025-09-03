93d73d0de8643d0ff43c08148539eb09e79ffe0f
anuri01 <yang1234@gmail.com>
2025-09-03 15:31:30 +0900

    feat: 관리자 전용 라우트 컴포넌트 추가

---
+++ client/src/components/AdminProtectedRoute.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Navigate } from 'react-router-dom';
+import useUserStore from "../store/userStore";
+
+// 로그인 및 관리자만 접속할 수 있도록하는 코드로 해당 페이지를 이 라우트로 감싸서 처리함
+// 이 컴포넌트는 'children'이라는 특별한 prop을 받습니다.
+// {children}은 이 라우트로 감싼 라우트를 가르킴
+// isLoggedIn 상태를 확인해 로그인한 상태면 페이지 접속을 그렇지 않을 경우 다른 페이지로 보냄
+function AdminProtectedRoute({children}) {
+    const { isLoggedIn, user } = useUserStore();
+    return isLoggedIn && user.role === 'admin' ? children : <Navigate to="/" />
+}
+
+export default AdminProtectedRoute;
